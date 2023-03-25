fbfff7bcdc13f8d21968d2de2acd05b2355f5bb6
Ramya0102 <ramyagajula42@gmail.com>
2023-03-25 15:37:06 +0530

    Navbar adding

---
+++ ramya/src/pages/Hobbies/index.js
@@ -0,0 +1,10 @@
+import Basiccard from "../../components/Basiccard";
+
+export default function Hobbies(){
+    return(
+        <>
+        This is Hobbies page
+        <Basiccard/>
+        </>
+    )
+}
